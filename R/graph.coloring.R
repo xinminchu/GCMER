32b161d54dc44af41ee1725c82c59be00f6b5095
David Degras <david.degras@umb.edu>
2024-02-29 16:12:44 +0100

    Update graph.coloring.R

--- R/graph.coloring.R
+++ R/graph.coloring.R
@@ -3,39 +3,27 @@
 # Wrapper function for graph coloring 
 ######################################
 
-graph.coloring <- function(adj.list, method = NULL)
+graph.coloring <- function(graph, 
+	method = c("lf", "sl", "dsatur", "rlf", "msc", "lmxrlf", "tabu"))
 {
-## Argument checks
-# stopifnot(is.list(adj.list))
-# stopifnot(is.numeric(unlist(adj.list)) || 
-	# is.character(unlist(adj.list)))
-# if (is.numeric(adj.list[[1]]))
-	# stopifnot(all(unlist(adj.list) %in% seq(1, length(adj.list))))
-# if (is.character(adj.list[[1]]))
-	# stopifnot(all(unlist(adj.list) %in% names(adj.list)))
-if (is.matrix(adj.list)) 
-	adj.list <- adjmat2list(adj.list)
-
-all.methods <- c("lf", "sl", "dsatur", "rlf", "msc", "lmxrlf", "tabu")
-if (is.null(method)) {
-	method <- all.methods[1:5]
-} else {
-	idx <- na.omit(pmatch(method, all.methods))
-	method <- all.methods[idx]	
-}
-if (length(method) == 0) 
-	stop("No valid values in 'method'.")
-out <- vector("list", length(method))
-names(out) <- method
+stopifnot(is.matrix(graph) || is.list(graph))
+if (is.matrix(graph)) {
+	test <- is.logical(graph) || all(graph %in% c(0,1))
+	graph <- if (test) adjmat2adjlist(graph) else edges2adjlist(graph)
+} 
+method <- match.arg(method, several.ok = TRUE)
+n <- length(graph)
+out <- matrix(0, n, length(method))
+colnames(out) <- method
 for (m in method)
-	out[[m]] <- switch(m, 
-		lf = graph_coloring_greedy(adj.list, "lf"),
-		sl = graph_coloring_greedy(adj.list, "sl"),
-		dsatur = graph_coloring_dsatur(adj.list),
-		rlf = graph_coloring_rlf(adj.list),
-		msc = graph_coloring_msc(adj.list),
-		lmxrlf = graph_coloring_lmxrlf(adj.list),
-		tabu = graph_coloring_tabucol(adj.list, ...))
+	out[,m] <- switch(m, 
+		lf = graph_coloring_greedy(graph, "lf"),
+		sl = graph_coloring_greedy(graph, "sl"),
+		dsatur = graph_coloring_dsatur(graph),
+		rlf = graph_coloring_rlf(graph),
+		msc = graph_coloring_msc(graph),
+		lmxrlf = graph_coloring_lmxrlf(graph),
+		tabu = graph_coloring_tabucol(graph, ...))
 
 out
 }		
